a0cb14191c0296b8880898e00ffaf8f85a8a2a0d
Janak Bhatt <bhattjanak59@gmail.com>
2024-09-27 16:28:26 +0545

    ticket count

---
+++ src/HEI.Support/Controllers/DashboardApiController.cs
@@ -0,0 +1,25 @@
+﻿using HEI.Support.Service.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HEI.Support.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardApiController : ControllerBase
+    {
+        private readonly ITicketService _ticketService;
+
+        public DashboardApiController(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+        [HttpGet]
+        [Route("GetAllTicketsCountAsync")]
+        public async Task<IActionResult> GetAllTicketsCountAsync(string? userId = null, DateTime? ticketDatetime = null)
+        {
+            var data = await _ticketService.GetAllTicketsCountAsync(userId, ticketDatetime);
+            return Ok(data);
+        }
+    }
+}
